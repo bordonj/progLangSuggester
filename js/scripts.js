b9cf0ec49bf1bab5bca55d91b576eb2812383ea1
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-04 17:54:11 -0700

    add result div id

--- js/scripts.js
+++ js/scripts.js
@@ -19,4 +19,24 @@ $(document).ready(function() {
     $('.firstQuestion').hide();
     $('.secondQuestion').fadeIn();
   })
+  $('#secondBack').click(function(e2) {
+    e2.preventDefault();
+    $('.secondQuestion').hide();
+    $('.firstQuestion').fadeIn();
+  })
+  $('#secondSubmit').click(function(e3) {
+    e3.preventDefault();
+    $('.secondQuestion').hide();
+    $('.thirdQuestion').fadeIn();
+  })
+  $('#thirdBack').click(function(e4) {
+    e4.preventDefault();
+    $('.thirdQuestion').hide();
+    $('.secondQuestion').fadeIn();
+  })
+  $('#thirdSubmit').click(function(e5) {
+    e5.preventDefault();
+    $('#survey').hide();
+    $('.intro').fadeIn();
+  })
 })
